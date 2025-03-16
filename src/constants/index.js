1301264984e87163cd0704bc9ff91d54fe1b7b77
Sohaib Shah <turbosohaib1234@gmail.com>
2025-03-16 13:59:55 +0500

    Updated content details

--- src/constants/index.js
+++ src/constants/index.js
@@ -91,7 +91,7 @@ export const PROJECTS = [
 ];
 
 export const CONTACT = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
+  address: "Haji Korona Baghdada Mardan, kpk, Pakistan",
+  phoneNo: "+92 3179949739",
+  email: "turbosohaib1234@gmail.com",
 };
